730429c4ab2469733cabe722a5090c40e1ed7fd5
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-09-21 15:37:23 -0500

    Prepare for redux

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
       <C.Modules.Input />
+      <C.Modules.Results />
     </>
   );
 }
--- src/components/Modules/Results.js
+++ src/components/Modules/Results.js
@@ -7,7 +7,12 @@ export default class Results extends React.Component {
     render() {
         return(
             <div className="results-container">
-                
+                <div className="result-item">
+                    <h2>RANK #</h2>
+                    <img src="" alt="MODE"/>
+                    <p>MODE</p>
+                    <p>Emission amount</p>
+                </div>
             </div>
         );
     }
