5de53700a52825055f069ec7f053df44395fbfca
Divya <divyaxg@gmail.com>
2019-09-21 19:25:42 -0500

    added address, state, city

--- src/components/Modules/Input.js
+++ src/components/Modules/Input.js
@@ -10,12 +10,16 @@ export default class Input extends React.Component {
                 <div className="input-column">
                     <form>
                         <div className="input-group">
-                            <input name="starting" type="text" placeholder="Starting location" />
-                            <button className="input-button">Find</button>
+                            <input name="beginAddress" type="text" placeholder="Street Address" />
+                            <input name="beginCity" type="text" placeholder="City" />
+                            <input name="beginState" type="text" placeholder="State" />
+                            //<button className="input-button">Find</button> Why do we have this button??
                         </div>
                         <div className="input-group last-input">
-                            <input name="target" type="text" placeholder="Target location" />
-                            <button className="input-button">Find</button>
+                            <input name="targetAddress" type="text" placeholder="Target Address" />
+                            <input name="targetCity" type="text" placeholder="Target City" />
+                            <input name="targetState" type="text" placeholder="Target State" />
+                          //  <button className="input-button">Find</button>
                         </div>
                     </form>
                 </div>
