de962379e83095be05fb4e6963681bdc5c92dbee
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-11-16 16:01:35 -0600

    Create Login.js

---
+++ src/components/Modules/Login.js
@@ -0,0 +1,42 @@
+// INPUT COMPONENT
+
+// REACT IMPORT
+import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { Actions, Selectors } from '../../data/current-user-id';
+
+
+const Login = () => {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+
+    const isLoading = useSelector(Selectors.getLoadingStatus);
+
+    const handleChange = (e) => {
+        setEmail(e.currentTarget.value);
+    }
+
+    const handleClick = () => {
+        dispatch(Actions.login(email));
+    }
+
+    return (
+        <div className="input-container">
+            <div className="input-column">
+                <div className="input-group">
+                    <input
+                        name="email"
+                        type="email"
+                        placeholder="Your email address"
+                        value={email}
+                        onChange={handleChange}
+                    />
+                    <button className="input-button" onClick={handleClick}>Login</button>
+                </div>
+            </div>
+            { isLoading && <div>Wait we're loading</div>}
+        </div>
+    );
+};
+
+export default Login;
