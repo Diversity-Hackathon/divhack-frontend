5396b9f3e9a9677e6390745f9c929d49c4c80912
Matthew Maxwell <maxwellmattryan@gmail.com>
2019-11-16 21:12:11 -0600

    Updates

---
+++ precache-manifest.e918586ca6955d86bc159ad93929e5c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacb9e3d19681683a93a45a4b0010927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b2d1b4f8642fb7f417a",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "5b2d1b4f8642fb7f417a",
+    "url": "/static/js/2.7ca54e63.chunk.js"
+  },
+  {
+    "revision": "30a0ba1dff8e6a74f279",
+    "url": "/static/js/main.67d2fd6a.chunk.js"
+  },
+  {
+    "revision": "475fa22902bde5ff1e67",
+    "url": "/static/js/runtime-main.31e01a45.js"
+  },
+  {
+    "revision": "c2fc88ef8b1207de6ae862da7caab2a0",
+    "url": "/static/media/logo.c2fc88ef.png"
+  }
+]);
